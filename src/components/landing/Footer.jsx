7009627838b7abc9abe836eed14b8246d963385c
Ugboizu <umezugbo@gmail.com>
2025-11-29 12:33:41 +0100

    social media changes

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import logoIcon from '../../assets/images/Logo Icon.png'
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook, FaYoutube } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
-import { FaInstagram } from "react-icons/fa6";
-import { FaLinkedin } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";;
 
 const Footer = () => {
   return (
@@ -16,11 +15,27 @@ const Footer = () => {
 			<div className='flex flex-col md:flex-row items-center justify-between gap-4 text-secondary pb-5'>
 				<p className='font-semibold md:font-medium text-xs md:text-lg'>The smart way to pay your bills</p>
 				<div className='flex gap-10'>
-					<a href='' className='hover:text-primary' ><FaFacebook size={25}/></a>
-					<a href='' className='hover:text-primary'><FaInstagram size={25}/></a>
-					<a href='' className='hover:text-primary'><FaXTwitter size={25}/></a>
-					<a href='' className='hover:text-primary'><FaLinkedin size={25}/></a>
+					<a href='https://www.facebook.com/BillWev' target='_blank' rel='noopener noreferrer'
+					className='hover:text-primary'> 
+						<FaFacebook size={25}/>
+					</a>
+
+					<a href='https://www.instagram.com/billwev' target='_blank' rel='noopener noreferrer'
+					className='hover:text-primary'>
+						<FaInstagram size={25}/>
+					</a>
+
+					<a href='https://twitter.com/BillWevApp' target='_blank' rel='noopener noreferrer'
+					className='hover:text-primary'>
+						<FaXTwitter size={25}/>
+					</a>
+
+					<a href='https://www.youtube.com/@BillWev' target='_blank' rel='noopener noreferrer'
+					className='hover:text-primary'>
+						<FaYoutube size={25}/>
+					</a>
 				</div>
+
 			</div>
 
 			<div className='flex flex-col md:flex-row items-center justify-between border-t border-[#1E1E1E] py-4 font-medium text-[#1E1E1E] text-[10px] md:text-lg'>
